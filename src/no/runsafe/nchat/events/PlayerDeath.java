e09f4c82ceb9031c35e09c8b0455dd9d85d24f29
Kruithne <kruithne@gmail.com>
2012-12-28 17:05:32 +0000

    Implemented basic death message hook.

---
+++ src/no/runsafe/nchat/events/PlayerDeath.java
@@ -0,0 +1,27 @@
+package no.runsafe.nchat.events;
+
+import no.runsafe.framework.event.entity.IEntityDeathEvent;
+import no.runsafe.framework.event.player.IPlayerDeathEvent;
+import no.runsafe.framework.server.event.entity.RunsafeEntityDeathEvent;
+import no.runsafe.framework.server.event.player.RunsafePlayerDeathEvent;
+import no.runsafe.framework.server.player.RunsafePlayer;
+import no.runsafe.nchat.handlers.ChatHandler;
+
+public class PlayerDeath implements IPlayerDeathEvent
+{
+	public PlayerDeath(ChatHandler chatHandler)
+	{
+		this.chatHandler = chatHandler;
+	}
+
+	@Override
+	public void OnPlayerDeathEvent(RunsafePlayerDeathEvent runsafePlayerDeathEvent)
+	{
+		// TODO: Work out how they died and allow message modification.
+		String originalMessage = runsafePlayerDeathEvent.getDeathMessage();
+		RunsafePlayer player = (RunsafePlayer) runsafePlayerDeathEvent.getEntity();
+		runsafePlayerDeathEvent.setDeathMessage(this.chatHandler.formatPlayerSystemMessage(originalMessage, player));
+	}
+
+	private ChatHandler chatHandler;
+}
