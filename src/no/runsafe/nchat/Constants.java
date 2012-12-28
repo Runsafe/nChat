f11aa7efe08f635ef285ed4f4a3fd6caf2ab0018
Kruithne <kruithne@gmail.com>
2012-12-28 11:39:42 +0000

    - Cleaned up structure - Extended mute command - Half-implemented unmute command - Few code improvement tweaks

--- src/no/runsafe/nchat/Constants.java
+++ src/no/runsafe/nchat/Constants.java
@@ -4,28 +4,32 @@ import org.bukkit.ChatColor;
 
 public class Constants
 {
-    public static String DEFAULT_CONFIGURATION_FILE = "defaultConfig.yml";
-    public static String CONFIGURATION_FILE = "plugins/nChat/config.yml";
+	public static String DEFAULT_CONFIGURATION_FILE = "defaultConfig.yml";
+	public static String CONFIGURATION_FILE = "plugins/nChat/config.yml";
 
-    public static String COLOR_CHARACTER = "&%s";
+	public static String COLOR_CHARACTER = "&%s";
 
-    public static String FORMAT_PLAYER_NAME = "#name";
-    public static String FORMAT_WORLD = "#world";
-    public static String FORMAT_GROUP = "#group";
-    public static String FORMAT_TAG = "#tags";
-    public static String FORMAT_MESSAGE = "#message";
-    public static String FORMAT_OP = "#op";
-    public static String FORMAT_CHANNEL = "#channel";
+	public static String FORMAT_PLAYER_NAME = "#name";
+	public static String FORMAT_WORLD = "#world";
+	public static String FORMAT_GROUP = "#group";
+	public static String FORMAT_TAG = "#tags";
+	public static String FORMAT_MESSAGE = "#message";
+	public static String FORMAT_OP = "#op";
+	public static String FORMAT_CHANNEL = "#channel";
 
-    public static ChatColor DEFAULT_MESSAGE_COLOR = ChatColor.AQUA;
+	public static ChatColor DEFAULT_MESSAGE_COLOR = ChatColor.AQUA;
 
-    public static String CHAT_CHANNEL_NODE = "nChat.channel.%s";
+	public static String CHAT_CHANNEL_NODE = "nChat.channel.%s";
 
-    public static String CHANNEL_NOT_EXIST = "The specified channel does not exist.";
-    public static String CHANNEL_NO_PERMISSION = "You do not have permission to speak in this channel.";
+	public static String CHANNEL_NOT_EXIST = "The specified channel does not exist.";
+	public static String CHANNEL_NO_PERMISSION = "You do not have permission to speak in this channel.";
 
-    public static String CHAT_MUTED = "Messages are currently not being broadcast.";
+	public static String CHAT_MUTED = "Messages are currently not being broadcast.";
 
-    public static String COMMAND_CHAT_MUTED = "Global chat has now been muted.";
-    public static String COMMAND_CHAT_UNMUTED = "Global chat has now been unmuted";
+	public static String COMMAND_CHAT_MUTED = "Global chat has now been muted.";
+	public static String COMMAND_CHAT_UNMUTED = "Global chat has now been unmuted";
+
+	public static String COMMAND_ENTER_PLAYER = "Please enter a player name";
+	public static String COMMAND_NO_PERMISSION = ChatColor.RED + "You do not have permission to do that.";
+	public static String COMMAND_TARGET_EXEMPT = ChatColor.RED + "You cannot use that on the specified player.";
 }
--- src/no/runsafe/nchat/Core.java
+++ src/no/runsafe/nchat/Core.java
@@ -2,30 +2,42 @@ package no.runsafe.nchat;
 
 import no.runsafe.framework.RunsafePlugin;
 import no.runsafe.framework.configuration.IConfigurationFile;
+import no.runsafe.nchat.command.ChannelCommand;
+import no.runsafe.nchat.command.MuteCommand;
+import no.runsafe.nchat.events.EventManager;
+import no.runsafe.nchat.handlers.ChatChannelHandler;
+import no.runsafe.nchat.handlers.ChatHandler;
+import no.runsafe.nchat.handlers.MuteHandler;
 
 import java.io.InputStream;
 
 public class Core extends RunsafePlugin implements IConfigurationFile
 {
-    @Override
-    protected void PluginSetup()
-    {
-        this.addComponent(Globals.class);
-        this.addComponent(ChatChannelHandler.class);
-        this.addComponent(ChatHandler.class);
-        this.addComponent(EventManager.class);
-        this.addComponent(ChannelCommand.class);
-    }
+	@Override
+	protected void PluginSetup()
+	{
+		// TODO: Implement anti-spam feature
+		// TODO: Implement Server join/leave messages
+		// TODO: Implement Death messages
 
-    @Override
-    public String getConfigurationPath()
-    {
-        return Constants.CONFIGURATION_FILE;
-    }
+		this.addComponent(Globals.class);
+		this.addComponent(ChatChannelHandler.class);
+		this.addComponent(ChatHandler.class);
+		this.addComponent(EventManager.class);
+		this.addComponent(MuteHandler.class);
+		this.addComponent(ChannelCommand.class);
+		this.addComponent(MuteCommand.class);
+	}
 
-    @Override
-    public InputStream getDefaultConfiguration()
-    {
-        return this.getResource(Constants.DEFAULT_CONFIGURATION_FILE);
-    }
+	@Override
+	public String getConfigurationPath()
+	{
+		return Constants.CONFIGURATION_FILE;
+	}
+
+	@Override
+	public InputStream getDefaultConfiguration()
+	{
+		return this.getResource(Constants.DEFAULT_CONFIGURATION_FILE);
+	}
 }
