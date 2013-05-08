01ac132c19d044b1d66ff1cab01021c75359e030
Kruithne <kruithne@gmail.com>
2013-05-08 22:00:28 +0100

    nChat no longer needs to edit the messages for kicks.

--- src/no/runsafe/nchat/events/KickEvent.java
+++ src/no/runsafe/nchat/events/KickEvent.java
@@ -1,19 +1,15 @@
 package no.runsafe.nchat.events;
 
-import no.runsafe.framework.configuration.IConfiguration;
-import no.runsafe.framework.event.IConfigurationChanged;
 import no.runsafe.framework.event.player.IPlayerKickEvent;
 import no.runsafe.framework.server.event.player.RunsafePlayerKickEvent;
 import no.runsafe.framework.server.player.RunsafePlayer;
-import no.runsafe.nchat.handlers.ChatHandler;
 import no.runsafe.nchat.handlers.SpamHandler;
 import no.runsafe.nchat.handlers.WhisperHandler;
 
-public class KickEvent implements IPlayerKickEvent, IConfigurationChanged
+public class KickEvent implements IPlayerKickEvent
 {
-	public KickEvent(ChatHandler chatHandler, WhisperHandler whisperHandler, SpamHandler spamHandler)
+	public KickEvent(WhisperHandler whisperHandler, SpamHandler spamHandler)
 	{
-		this.chatHandler = chatHandler;
 		this.whisperHandler = whisperHandler;
 		this.spamHandler = spamHandler;
 	}
@@ -22,28 +18,10 @@ public class KickEvent implements IPlayerKickEvent, IConfigurationChanged
 	public void OnPlayerKick(RunsafePlayerKickEvent runsafePlayerKickEvent)
 	{
 		RunsafePlayer kickedPlayer = runsafePlayerKickEvent.getPlayer();
-		runsafePlayerKickEvent.setLeaveMessage(
-			(this.suppressKickMessages)
-				? null
-				: this.chatHandler.formatPlayerSystemMessage(
-						this.kickMessage.replace("#reason", runsafePlayerKickEvent.getReason()),
-						kickedPlayer
-					)
-		);
 		this.spamHandler.flushPlayer(kickedPlayer);
 		this.whisperHandler.deleteLastWhisperedBy(kickedPlayer);
 	}
 
-	@Override
-	public void OnConfigurationChanged(IConfiguration iConfiguration)
-	{
-		this.kickMessage = iConfiguration.getConfigValueAsString("chatMessage.kicked");
-		this.suppressKickMessages = iConfiguration.getConfigValueAsBoolean("spamControl.suppressKickMessages");
-	}
-
-	private String kickMessage;
-	private boolean suppressKickMessages;
-	private final ChatHandler chatHandler;
 	private final WhisperHandler whisperHandler;
 	private final SpamHandler spamHandler;
 }
