52dddc8090c70d7175295d83f0dfdc7756e9c1fe
Kruithne <kruithne@gmail.com>
2013-06-17 22:11:00 +0100

    [Debug] Output the maths we are calculating.

--- src/no/runsafe/nchat/antispam/CapsFilter.java
+++ src/no/runsafe/nchat/antispam/CapsFilter.java
@@ -1,6 +1,7 @@
 package no.runsafe.nchat.antispam;
 
 import no.runsafe.framework.api.IConfiguration;
+import no.runsafe.framework.api.IOutput;
 import no.runsafe.framework.api.event.plugin.IConfigurationChanged;
 import no.runsafe.framework.minecraft.player.RunsafePlayer;
 
@@ -9,8 +10,9 @@ import java.util.regex.Pattern;
 
 public class CapsFilter implements ISpamFilter, IConfigurationChanged
 {
-	public CapsFilter()
+	public CapsFilter(IOutput output)
 	{
+		this.output = output;
 		this.caps = Pattern.compile("[A-Z]");
 	}
 
@@ -23,6 +25,9 @@ public class CapsFilter implements ISpamFilter, IConfigurationChanged
 			int count = 0;
 			while (matcher.find()) count++;
 
+			this.output.write("Found " + count + " caps.");
+			this.output.write("Message length: " + message.length());
+			this.output.write("Percent: " + (count * 100) / message.length());
 			if ((count * 100) / message.length() >= this.percent)
 				return message.toLowerCase();
 		}
@@ -39,4 +44,5 @@ public class CapsFilter implements ISpamFilter, IConfigurationChanged
 	private boolean isEnabled;
 	private int percent;
 	private Pattern caps;
+	private IOutput output;
 }
