fc160ce17d0dd35c2c9989b04834cfcd32263a7c
Kruithne <kruithne@gmail.com>
2013-09-16 16:30:06 +0100

    Implemented test emote system.

--- src/no/runsafe/nchat/emotes/EmoteHandler.java
+++ src/no/runsafe/nchat/emotes/EmoteHandler.java
@@ -1,10 +1,22 @@
 package no.runsafe.nchat.emotes;
 
+import no.runsafe.framework.api.IConfiguration;
 import no.runsafe.framework.api.IOutput;
 import no.runsafe.framework.api.event.player.IPlayerCommandPreprocessEvent;
+import no.runsafe.framework.api.event.plugin.IConfigurationChanged;
+import no.runsafe.framework.minecraft.RunsafeServer;
 import no.runsafe.framework.minecraft.event.player.RunsafePlayerCommandPreprocessEvent;
+import no.runsafe.framework.minecraft.player.RunsafeAmbiguousPlayer;
+import no.runsafe.framework.minecraft.player.RunsafePlayer;
 
-public class EmoteHandler implements IPlayerCommandPreprocessEvent
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmoteHandler implements IPlayerCommandPreprocessEvent, IConfigurationChanged
 {
 	public EmoteHandler(IOutput output)
 	{
@@ -12,66 +24,87 @@ public class EmoteHandler implements IPlayerCommandPreprocessEvent
 	}
 
 	@Override
+	public void OnConfigurationChanged(IConfiguration iConfiguration)
+	{
+		this.emotes.clear();
+		String path = "plugins/nChat/";
+		File pathDir = new File(path);
+		if (!pathDir.exists())
+		{
+			if (!pathDir.mkdirs())
+			{
+				output.logError("Could not create directory tree for nChat emotes!");
+				return;
+			}
+		}
+
+		File file = new File(path + "emotes.txt");
+		if (!file.exists())
+		{
+			try
+			{
+				if (!file.createNewFile())
+				{
+					output.logError("Could not create emotes file.");
+					return;
+				}
+
+				BufferedReader br = new BufferedReader(new FileReader(file));
+				String currentLine;
+
+				while ((currentLine = br.readLine()) != null)
+					emotes.add(new Emote(currentLine));
+				br.close();
+
+				this.output.logInformation("Loaded %s emotes!", emotes.size());
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@Override
 	public void OnBeforePlayerCommand(RunsafePlayerCommandPreprocessEvent event)
 	{
-		output.fine("Command message: " + event.getMessage());
+		if (event.isCancelled())
+			return;
+
+		String[] parts = event.getMessage().split(" ");
+		for (Emote emote : emotes)
+		{
+			if (parts[0].equalsIgnoreCase("/" + emote.getEmote()))
+			{
+				RunsafePlayer targetPlayer = parts.length > 1 ? RunsafeServer.Instance.getPlayer(parts[1]) : null;
+				this.broadcastEmote(emote, event.getPlayer(),  targetPlayer);
+				break;
+			}
+		}
+	}
+
+	private void broadcastEmote(Emote emote, RunsafePlayer player, RunsafePlayer target)
+	{
+		if (target instanceof RunsafeAmbiguousPlayer)
+		{
+			player.sendColouredMessage(target.toString());
+			return;
+		}
+
+		if (target != null)
+		{
+			RunsafeServer.Instance.broadcastMessage(String.format(
+					emote.getTargetEmote(), player.getPrettyName(), target.getPrettyName()
+			));
+		}
+		else
+		{
+			RunsafeServer.Instance.broadcastMessage(String.format(
+					emote.getSingleEmote(), player.getPrettyName()
+			));
+		}
 	}
 
 	private IOutput output;
-	private String[][] emotes = {
-			{"bite", "%s bites their arm.", "%s runs over and bites %s."},
-			{"kiss", "%s wants to be kissed.", "%s blows a kiss to %s."},
-			{"slap", "%s slaps themself across the face.", "%s slaps %s in the face."},
-			{"clap", "%s claps their hands.", "%s claps for %s."},
-			{"cry", "%s cries.", "%s cries on %s's shoulder."},
-			{"dropkick", "%s wants to be drop-kicked.", "%s drop-kicks %s to China."},
-			{"wink", "%s winks.", "%s winks at %s."},
-			{"wave", "%s waves .", "%s waves to %s."},
-			{"look", "%s looks at themself.", "%s looks at %s."},
-			{"sleep", "%s falls asleep.", "%s falls asleep on %s."},
-			{"cheer", "%s cheers.", "%s cheers for %s."},
-			{"smirk", "%s smirks.", "%s smirks at %s."},
-			{"smile", "%s smiles.", "%s smiles at %s."},
-			{"frown", "%s frowns.", "%s frowns at %s."},
-			{"run", "%s runs away.", "%s runs away from %s."},
-			{"dance", "%s dances.", "%s dances with %s."},
-			{"yawn", "%s yawns.", "%s yawns at %s."},
-			{"creep", "%s creeps around.", "%s creeps on %s."},
-			{"tongue", "%s sticks out tongue.", "%s sticks tongue out at %s."},
-			{"hug", "%s hugs themself.", "%s hugs %s."},
-			{"bow", "%s takes a bow.", "%s bows down to %s."},
-			{"drool", "%s drools.", "%s drools over %s."},
-			{"blush", "%s blushes.", "%s blushes at %s."},
-			{"punch", "%s punches their face.", "%s punches %s in the face."},
-			{"stumble", "%s stumbles.", "%s stumbles over %s."},
-			{"touch", "%s touches themself.", "%s touches %s."},
-			{"poke", "%s pokes their eye.", "%s pokes %s."},
-			{"die", "%s died.", "%s dies on %s."},
-			{"skip", "%s skips around.", "%s skips around %s."},
-			{"hide", "%s hides.", "%s hides from %s."},
-			{"sing", "%s sings.", "%s sings to %s."},
-			{"panic", "%s runs around in a panic.", "%s panics with %s."},
-			{"whistle", "%s whistles.", "%s whistles at %s."},
-			{"growl", "%s growls.", "%s growls at %s."},
-			{"wait", "%s waits.", "%s waits for %s."},
-			{"roar", "%s roars.", "%s roars at %s."},
-			{"sigh", "%s sighs.", "%s sighs at %s."},
-			{"giggle", "%s giggles.", "%s giggles at %s."},
-			{"point", "%s points.", "%s points to %s."},
-			{"eats", "%s eats their food.", "%s eats %s."},
-			{"throw", "%s throws their room across the lunch.", "%s throws %s."},
-			{"facepalm", "%s face-palms.", "%s face-palms because of %s."},
-			{"shake", "%s shakes head.", "%s shakes head at %s."},
-			{"glare", "%s glares.", "%s glares at %s."},
-			{"cringe", "%s cringes.", "%s cringes at %s."},
-			{"gasp", "%s gasps.", "%s gasps at %s."},
-			{"highfive", "%s high-fives themself.", "%s jumps and high-fives %s."},
-			{"lay", "%s lays down.", "%s lays down on %s."},
-			{"jump", "%s jumps up and down.", "%s jumps on top of %s."},
-			{"shrug", "%s shrugs.", "%s shrugs their shoulders at %s."},
-			{"quit", "%s throws arms up and quits.", "%s quits with %s."},
-			{"batman", "%s transforms into Batman.", "%s turns %s into Batman."},
-			{"meow", "%s meows.", "%s meows at %s."},
-			{"flip", "%s flips the table.", "%s flips %s over."},
-	};
+	private List<Emote> emotes = new ArrayList<Emote>();
 }
