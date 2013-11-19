dc75337f8683a41bcb5f7e9a0aac5aaa687972e9
Kruithne <kruithne@gmail.com>
2013-11-19 00:35:25 +0000

    Don't broadcast death messages in worlds configured to be ignored.

--- src/no/runsafe/nchat/chat/DeathHandler.java
+++ src/no/runsafe/nchat/chat/DeathHandler.java
@@ -5,6 +5,7 @@ import no.runsafe.framework.api.event.player.IPlayerDeathEvent;
 import no.runsafe.framework.api.event.plugin.IConfigurationChanged;
 import no.runsafe.framework.files.PluginDataFile;
 import no.runsafe.framework.files.PluginFileManager;
+import no.runsafe.framework.minecraft.RunsafeWorld;
 import no.runsafe.framework.minecraft.event.player.RunsafePlayerDeathEvent;
 
 import java.util.List;
@@ -22,12 +23,13 @@ public class DeathHandler implements IPlayerDeathEvent, IConfigurationChanged
 	public void OnConfigurationChanged(IConfiguration config)
 	{
 		messages = deathMessageFile.getLines();
+		ignoreWorlds = config.getConfigValueAsList("hideDeaths");
 	}
 
 	@Override
 	public void OnPlayerDeathEvent(RunsafePlayerDeathEvent event)
 	{
-		if (!messages.isEmpty()) // We have no messages!
+		if (!messages.isEmpty() && canBroadcastHere(event.getEntity().getWorld())) // We have no messages!
 		{
 			event.setDeathMessage(""); // Set the Minecraft death message to blank to silence output
 			String message = messages.get(random.nextInt(messages.size())); // Get a random death message.
@@ -35,8 +37,14 @@ public class DeathHandler implements IPlayerDeathEvent, IConfigurationChanged
 		}
 	}
 
+	private boolean canBroadcastHere(RunsafeWorld world)
+	{
+		return world != null && !ignoreWorlds.contains(world.getName());
+	}
+
 	private PluginDataFile deathMessageFile;
 	private List<String> messages;
+	private List<String> ignoreWorlds;
 	private Random random = new Random();
 	private final ChatEngine chatEngine;
 }
