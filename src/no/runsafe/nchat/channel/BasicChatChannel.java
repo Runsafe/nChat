0bd1a0b1dec55ab99af98081943ac815782f3ce5
Morten Nilsen <morten@runsafe.no>
2014-06-09 20:00:55 +0200

    Allow fake messages onto any channel

--- src/no/runsafe/nchat/channel/BasicChatChannel.java
+++ src/no/runsafe/nchat/channel/BasicChatChannel.java
@@ -40,7 +40,7 @@ public class BasicChatChannel implements IChatChannel
 	@Override
 	public void Send(RunsafePlayerChatEvent message)
 	{
-		if (!members.containsKey(message.getPlayer().getName()))
+		if (!members.containsKey(message.getPlayer().getName()) || message.isFake())
 			return;
 
 		String incoming = manager.filter(message.getPlayer(), message.getMessage());
