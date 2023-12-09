9b2810aef6405a38d15f1c9bd0e1799637b5e2cd
DevDeborah <softwaredeveloperdeborah@gmail.com>
2023-12-08 23:23:14 -0500

    Use new Duration command argument

--- src/no/runsafe/nchat/command/MuteCommand.java
+++ src/no/runsafe/nchat/command/MuteCommand.java
@@ -3,12 +3,10 @@ package no.runsafe.nchat.command;
 import no.runsafe.framework.api.command.ExecutableCommand;
 import no.runsafe.framework.api.command.ICommandExecutor;
 import no.runsafe.framework.api.command.argument.IArgumentList;
-import no.runsafe.framework.api.command.argument.Period;
+import no.runsafe.framework.api.command.argument.Duration;
 import no.runsafe.framework.api.command.argument.Player;
 import no.runsafe.framework.api.player.IPlayer;
 import no.runsafe.nchat.chat.MuteHandler;
-
-import java.time.Duration;
 
 public class MuteCommand extends ExecutableCommand
 {
@@ -16,7 +14,7 @@ public class MuteCommand extends ExecutableCommand
 	{
 		super(
 			"mute", "Suppress chat messages from a player", "runsafe.nchat.mute",
-			new Player().require(), new Period()
+			new Player().require(), new Duration()
 		);
 
 		this.muteHandler = muteHandler;
@@ -27,10 +25,8 @@ public class MuteCommand extends ExecutableCommand
 	{
 		IPlayer player = executor instanceof IPlayer ? (IPlayer) executor : null;
 		IPlayer mutePlayer = parameters.getRequired("player");
-		org.joda.time.Period durationJoda = parameters.getValue("duration");
-		Duration duration = null;
-		if (durationJoda != null)
-			duration = Duration.ofMillis(durationJoda.getMillis());
+
+		java.time.Duration duration = parameters.getValue("duration");
 
 		if (player != null && mutePlayer.hasPermission("runsafe.nchat.mute.exempt"))
 			return "&cNice try, but you cannot mute that player."; // Unless you are the console ^w^
