2d12fac992ff3de2416d4ed12b2c48a029b93b38
Morten Nilsen <morten@runsafe.no>
2024-03-12 22:41:45 +0100

    Enhanced player data api

--- src/no/runsafe/nchat/PlayerDataProvider.java
+++ src/no/runsafe/nchat/PlayerDataProvider.java
@@ -1,14 +1,13 @@
 package no.runsafe.nchat;
 
 import no.runsafe.framework.api.hook.IPlayerDataProvider;
+import no.runsafe.framework.api.hook.PlayerData;
 import no.runsafe.framework.api.player.IPlayer;
 import no.runsafe.nchat.chat.IgnoreHandler;
 import no.runsafe.nchat.chat.MuteHandler;
 import org.apache.commons.lang.StringUtils;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class PlayerDataProvider implements IPlayerDataProvider
@@ -20,23 +19,31 @@ public class PlayerDataProvider implements IPlayerDataProvider
 	}
 
 	@Override
-	public Map<String, String> GetPlayerData(IPlayer player)
+	public void GetPlayerData(PlayerData data)
 	{
-		Map<String, String> data = new HashMap<>(3);
-
-		data.put("nchat.isMuted", muteHandler.isPlayerMuted(player) ? "True" : "False");
-
-		List<IPlayer> ignoringPlayers = ignoreHandler.getPlayersIgnoring(player);
-		data.put("nchat.ignoredBy", ignoringPlayers.isEmpty() ? "Nobody" : StringUtils.join(
-			ignoringPlayers.stream().map(IPlayer::getName).collect(Collectors.toList()), ", "
-		));
-
-		List<IPlayer> ignoredPlayers = ignoreHandler.getIgnoredPlayers(player);
-		data.put("nchat.ignoring", ignoredPlayers.isEmpty() ? "Nobody" : StringUtils.join(
-			ignoredPlayers.stream().map(IPlayer::getName).collect(Collectors.toList()), ", "
-		));
-
-		return data;
+		data.addData("nchat.isMuted", () -> muteHandler.isPlayerMuted(data.getPlayer()) ? "True" : "False");
+		data.addData(
+			"nchat.ignoredBy",
+			() ->
+			{
+				List<IPlayer> ignoringPlayers = ignoreHandler.getPlayersIgnoring(data.getPlayer());
+				return ignoringPlayers.isEmpty() ? "Nobody" : StringUtils.join(
+					ignoringPlayers.stream().map(IPlayer::getName).collect(Collectors.toList()),
+					", "
+				);
+			}
+		);
+		data.addData(
+			"nchat.ignoring",
+			() ->
+			{
+				List<IPlayer> ignoredPlayers = ignoreHandler.getIgnoredPlayers(data.getPlayer());
+				return ignoredPlayers.isEmpty() ? "Nobody" : StringUtils.join(
+					ignoredPlayers.stream().map(IPlayer::getName).collect(Collectors.toList()),
+					", "
+				);
+			}
+		);
 	}
 
 	private final MuteHandler muteHandler;
